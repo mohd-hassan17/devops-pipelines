084fe17fd3c1063d7e7c45a4a6084ed804783333
mohd-hassan17 <mohdhassana04@gmail.com>
2026-04-28 21:10:56 +0530

    new changes

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 console.log("hii there!");
 setTimeout(() => {
   console.log("This is a test file for devops pipelines.");
-}, 3000);
-console.log("waiting for 3 seconds...");
+}, 2000);
+console.log("waiting for 2 seconds...");
